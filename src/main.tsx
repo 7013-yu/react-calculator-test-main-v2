30b70a6fb641574c18e5b27b90aff5f52ee95c56
Yu <judy940331@gmail.com>
2025-12-18 15:51:23 +0800

    done

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { MyHook } from './MyHook.tsx'
+import MyGame from './MyGame.tsx'
 // import { MyView } from './MyView.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App/>
+    <MyGame />
     {/* <MyHook/> */}
     {/* <MyView/> */}
     
